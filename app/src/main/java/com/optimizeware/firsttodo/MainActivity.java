4161ead9bc3cd417184d51a0a3204d62f666f0ca
Nasr Mohd <nasreldeenmohamed@gmail.com>
2018-02-12 21:25:03 +0200

    Modified MainActivity Added MyClass

--- app/src/main/java/com/optimizeware/firsttodo/MainActivity.java
+++ app/src/main/java/com/optimizeware/firsttodo/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "settings are not developed yet!", Toast.LENGTH_SHORT).show();
             return true;
         }
 
---
+++ app/src/main/java/com/optimizeware/firsttodo/MyClass.java
@@ -0,0 +1,8 @@
+package com.optimizeware.firsttodo;
+
+/**
+ * Created by Nasr on 2/12/2018.
+ */
+
+public class MyClass {
+}
